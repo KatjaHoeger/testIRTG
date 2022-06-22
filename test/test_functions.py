0bacd59a8a6da5472fffacd53efa54e262f28de0
KatjaHoeger <93534080+KatjaHoeger@users.noreply.github.com>
2022-06-22 11:41:46 +0200

    Parametrize our test function

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,7 +1,10 @@
 from functions import add
 import numpy as np
 
-def test_add():
-  assert add(3,4)==7
-  
+@pytest.mark.parametrize(
+  "x, y, result", [(1,2,3),(3,4,7), (4,5,9)]
+  )
+def test_add(x,y,result):
+  assert add(x,y) ==result
+
 
